36fb4359773ef2b35efad9870c53f79c9f6c7b73
Rob Chamberlain <robert.chamberlain@vaimo.com>
2022-11-13 21:12:17 +0100

    Initial commit

---
+++ js/loops.js
@@ -0,0 +1,8 @@
+// don't touch the window.addEventListener line, or the closing }); at the end of the file
+window.addEventListener("load", function () {
+  /* 
+    Excercise 7, 10 and 11 require prompts, everything else requires console logging. 
+    Feel free to comment out completed code to stop the previous exercises repeating
+  */
+    
+});
