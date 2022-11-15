068961405211b9205407a2dbff5dcbb8a117e8ae
Rob Chamberlain <robert.chamberlain@vaimo.com>
2022-11-15 21:23:53 +0100

    Arrays

---
+++ js/arrays.js
@@ -0,0 +1,19 @@
+// don't touch the window.addEventListener line, or the closing }); at the end of the file
+window.addEventListener("load", function () {
+// don't touch this array
+  let countryArray = [
+    "Australia", 
+    "Chile", 
+    "Denmark", 
+    "France", 
+    "Germany", 
+    "India", 
+    "Japan", 
+    "Morrocco",
+    "New Zealand",
+    "Portugal",
+    "Sweden",
+    "Turkey"
+  ];
+
+});
